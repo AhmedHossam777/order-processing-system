42a411abec827892b2c80754f0237b9ee86512c2
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-03-11 21:23:55 +0200

    feat(server): finish  shared lib

---
+++ libs/shared/src/index.ts
@@ -0,0 +1,5 @@
+export * from './events/order-created.event';
+export * from './events/payment-success.event';
+export * from './events/payment-failed.event';
+
+export * from './rabbitmq/rabbitmq.constants';
