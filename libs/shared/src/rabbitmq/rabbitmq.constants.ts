de45f47baaa35a28d60fe9163539386a47ac02c8
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-03-11 21:18:26 +0200

    feat(server): add rabbitmq constants

---
+++ libs/shared/src/rabbitmq/rabbitmq.constants.ts
@@ -0,0 +1,18 @@
+export const RABBITMQ_URL = 'amqp://admin:password@localhost:5672';
+
+// Queues names
+export const ORDER_QUEUE = 'order_event_queue';
+export const PAYMENT_QUEUE = 'payment_event_queue';
+export const NOTIFICATION = 'notifaction_event_queue';
+
+// Routing keys
+export const ROUTING_KEYS = {
+  ORDER_CREATED: 'order.created',
+  ORDER_NOTIFICATION: 'order.notification',
+  PAYMENT_SUCCESS: 'payment.success',
+  PAYMENT_FAILED: 'payment.failed',
+};
+
+// Injection tokens
+export const PAYMENT_CLIENT = 'PAYMENT_CLIENT';
+export const NOTIFICATION_CLIENT = 'NOTIFICATION_CLIENT';
