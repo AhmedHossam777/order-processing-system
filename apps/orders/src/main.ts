497c9997ba05a6e1b5e185d128e9a046899ee49c
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-03-11 04:12:09 +0200

    refactor(server): refactor the whole strucure

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { OrdersModule } from './orders.module';
+import { OrdersModule } from './orders/orders.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { PaymentsModule } from './payments.module';
+import { PaymentsModule } from './orders/payments.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PaymentsModule);
