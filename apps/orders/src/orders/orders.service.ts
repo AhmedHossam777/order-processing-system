8e16c0d4e742f2486c803a33d3868b09e4eb7228
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-03-12 17:55:42 +0200

    feat(server): add order service

--- apps/orders/src/orders/orders.service.ts
+++ apps/orders/src/orders/orders.service.ts
@@ -1,8 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { Repository } from 'typeorm';
+import {
+  NOTIFICATION_CLIENT,
+  OrderCreatedEvent,
+  PAYMENT_CLIENT,
+  ROUTING_KEYS,
+} from '@app/shared';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateOrderDto } from './dto/create-order.dto';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly logger = new Logger(OrdersService.name);
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+
+    @Inject(PAYMENT_CLIENT)
+    private readonly paymentClient: ClientProxy,
+
+    @Inject(NOTIFICATION_CLIENT)
+    private readonly notificationClient: ClientProxy,
+  ) {}
+
+  async createOrder(createOrderDto: CreateOrderDto): Promise<Order> {
+    const order = this.orderRepository.create({
+      ...createOrderDto,
+      status: 'PENDING',
+    });
+
+    const savedOrder = await this.orderRepository.save(order);
+    this.logger.log(`Order saved to DB: ${savedOrder.id}`);
+
+    // creating the order event
+    const createOrderEvent = new OrderCreatedEvent(
+      savedOrder.id,
+      savedOrder.product,
+      savedOrder.quantity,
+      savedOrder.price,
+      savedOrder.userId,
+    );
+
+    this.paymentClient.emit(ROUTING_KEYS.ORDER_CREATED, createOrderEvent);
+    this.notificationClient.emit(
+      ROUTING_KEYS.ORDER_NOTIFICATION,
+      createOrderEvent,
+    );
+
+    this.logger.log(`Events published for order: ${savedOrder.id}`);
+    return savedOrder;
+  }
+
+  async updateOrderStatus(orderId: string, status: string): Promise<void> {
+    await this.orderRepository.update(orderId, { status });
+    this.logger.log(`Order ${orderId} status → ${status}`);
+  }
+
+  async findAll(): Promise<Order[]> {
+    return this.orderRepository.find({
+      order: { createdAt: 'DESC' },
+    });
+  }
+
+  async findOne(id: string): Promise<Order> {
+    return this.orderRepository.findOne({ where: { id } });
   }
 }
