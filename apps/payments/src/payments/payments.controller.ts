2a1f06ba7d5f6588e6bbbadbed141df8d8b30489
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-03-14 01:40:56 +0200

    feat(server):add payment controller and service

--- apps/payments/src/payments/payments.controller.ts
+++ apps/payments/src/payments/payments.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { OrderCreatedEvent, ROUTING_KEYS } from '@app/shared';
 
 @Controller()
 export class PaymentsController {
+  private readonly logger = new Logger(PaymentsController.name);
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  @Get()
-  getHello(): string {
-    return this.paymentsService.getHello();
+  @EventPattern(ROUTING_KEYS.ORDER_CREATED)
+  async handleOrderCreated(
+    @Payload() event: OrderCreatedEvent,
+    @Ctx() context: RmqContext,
+  ) {
+    const originalMessage = context.getMessage();
+    const channel = context.getChannelRef();
+
+    try {
+      await this.paymentsService.processOrderPayment(event);
+      channel.ack(originalMessage);
+      this.logger.log(`ACK: order ${event.orderId}`);
+    } catch (e) {
+      this.logger.error(`Payment processing failed `, e);
+      channel.nack(originalMessage, false, false);
+    }
   }
 }
