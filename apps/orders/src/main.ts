55193fe6d3bffcf410fc1fa46c295755a02e052b
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-03-13 08:22:59 +0200

    feat(server): finish order app

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
-import { OrdersModule } from './orders/orders.module';
+import { AppModule } from './app.module';
+import { ORDER_QUEUE, RABBITMQ_URL } from '@app/shared';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(OrdersModule);
+  const logger = new Logger();
+
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [RABBITMQ_URL],
+      queue: ORDER_QUEUE,
+      queueOptions: { durable: true },
+      noAck: false,
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(process.env.port ?? 3000);
+
+  logger.log('Orders Service running on http://localhost:3000');
+  logger.log('Consuming from: order_events_queue');
 }
 bootstrap();
--- apps/orders/src/orders/orders.module.ts
+++ apps/orders/src/orders/orders.module.ts
@@ -1,9 +1,42 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import {
+  NOTIFICATION_CLIENT,
+  NOTIFICATION_QUEUE,
+  PAYMENT_CLIENT,
+  PAYMENT_QUEUE,
+  RABBITMQ_URL,
+} from '@app/shared';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([Order]),
+
+    ClientsModule.register([
+      {
+        name: PAYMENT_CLIENT,
+        transport: Transport.RMQ,
+        options: {
+          urls: [RABBITMQ_URL],
+          queue: PAYMENT_QUEUE,
+          queueOptions: { durable: true },
+        },
+      },
+      {
+        name: NOTIFICATION_CLIENT,
+        transport: Transport.RMQ,
+        options: {
+          urls: [RABBITMQ_URL],
+          queue: NOTIFICATION_QUEUE,
+          queueOptions: { durable: true },
+        },
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
