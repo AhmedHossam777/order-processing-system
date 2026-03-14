f95d260da319349be4404d5f5c876138ef01f2ec
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-03-14 06:56:16 +0200

    feat(notification): finish notification

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
-import { NotificationsModule } from './notifications/notifications.module';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { NOTIFICATION_QUEUE, RABBITMQ_URL } from '@app/shared';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NotificationsModule);
-  await app.listen(process.env.port ?? 3000);
+  const logger = new Logger('NotificationsService');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [RABBITMQ_URL],
+        queue: NOTIFICATION_QUEUE,
+        queueOptions: { durable: true },
+        noAck: false,
+      },
+    },
+  );
+  await app.listen();
+
+  logger.log('Notifications Service started');
+  logger.log('Consuming from: notification_events_queue');
 }
 bootstrap();
