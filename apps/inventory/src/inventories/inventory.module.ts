8eb82e84575ddee590b2c9e0d2f35c7600c7b799
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-03-15 03:04:02 +0200

    feat(inventory):add module

--- apps/inventory/src/inventories/inventory.module.ts
+++ apps/inventory/src/inventories/inventory.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { InventoryController } from './inventory.controller';
 import { InventoryService } from './inventory.service';
+import {
+  RABBITMQ_URL,
+  ORDER_QUEUE,
+  PAYMENT_QUEUE,
+  ORDER_CLIENT,
+  PAYMENT_CLIENT,
+} from '@app/shared';
+import { Reservation } from './entities/reservations.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([Reservation]),
+    ClientsModule.register([
+      {
+        name: ORDER_CLIENT,
+        transport: Transport.RMQ,
+        options: {
+          urls: [RABBITMQ_URL],
+          queue: ORDER_QUEUE,
+          queueOptions: { durable: true },
+        },
+      },
+      {
+        name: PAYMENT_CLIENT,
+        transport: Transport.RMQ,
+        options: {
+          urls: [RABBITMQ_URL],
+          queue: PAYMENT_QUEUE,
+          queueOptions: { durable: true },
+        },
+      },
+    ]),
+  ],
   controllers: [InventoryController],
   providers: [InventoryService],
 })
