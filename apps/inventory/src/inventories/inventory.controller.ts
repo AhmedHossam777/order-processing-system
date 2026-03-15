2d66fc2b99e75723d6d9c3bb5575ccba67d9e99b
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-03-15 03:02:52 +0200

    feat(inventory):add service and controller

--- apps/inventory/src/inventories/inventory.controller.ts
+++ apps/inventory/src/inventories/inventory.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { PaymentSuccessEvent, ROUTING_KEYS } from '@app/shared';
 
 @Controller()
 export class InventoryController {
+  private readonly logger = new Logger(InventoryController.name);
+
   constructor(private readonly inventoryService: InventoryService) {}
 
-  @Get()
-  getHello(): string {
-    return this.inventoryService.getHello();
+  @EventPattern(ROUTING_KEYS.PAYMENT_SUCCESS)
+  async handlePaymentSuccess(
+    @Payload() event: PaymentSuccessEvent,
+    @Ctx() context: RmqContext,
+  ) {
+    const originalMessage = context.getMessage();
+    const channel = context.getChannelRef();
+
+    try {
+      await this.inventoryService.reserveStock(event);
+      channel.ack(originalMessage);
+    } catch (e) {
+      this.logger.error('Inventory error', e);
+      channel.nack(originalMessage, false, false);
+    }
   }
 }
--- apps/inventory/src/inventories/inventory.service.ts
+++ apps/inventory/src/inventories/inventory.service.ts
@@ -1,8 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Reservation } from './entities/reservations.entity';
+import { Repository } from 'typeorm';
+import {
+  InventoryFailedEvent,
+  InventoryReservedEvent,
+  ORDER_CLIENT,
+  PAYMENT_CLIENT,
+  PaymentSuccessEvent,
+  ROUTING_KEYS,
+} from '@app/shared';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class InventoryService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly logger = new Logger(InventoryService.name);
+
+  constructor(
+    @InjectRepository(Reservation)
+    private readonly reservationRepository: Repository<Reservation>,
+
+    @Inject(ORDER_CLIENT) private readonly orderClient: ClientProxy,
+    @Inject(PAYMENT_CLIENT) private readonly paymentClient: ClientProxy,
+  ) {}
+
+  async reserveStock(event: PaymentSuccessEvent) {
+    this.logger.log(`Attempting to reserve stock for order: ${event.orderId}`);
+    const inStock = Math.random() > 0.3;
+
+    if (inStock) {
+      const reservation = this.reservationRepository.create({
+        orderId: event.orderId,
+        status: 'RESERVED',
+      });
+
+      await this.reservationRepository.save(reservation);
+
+      this.orderClient.emit(
+        ROUTING_KEYS.INVENTORY_RESERVED,
+        new InventoryReservedEvent(event.orderId),
+      );
+    } else {
+      const reason = 'Out of stock';
+      const reservation = this.reservationRepository.create({
+        orderId: event.orderId,
+        status: 'FAILED',
+        reason,
+      });
+      await this.reservationRepository.save(reservation);
+
+      this.logger.warn(
+        `OUT OF STOCK for order: ${event.orderId} (DB ID: ${reservation.id})`,
+      );
+
+      this.paymentClient.emit(
+        ROUTING_KEYS.INVENTORY_FAILED,
+        new InventoryFailedEvent(event.orderId, 'Unknown Product', reason),
+      );
+    }
   }
 }
